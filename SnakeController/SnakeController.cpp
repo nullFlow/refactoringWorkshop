d284a696e2b6ca5ec4ce7a809309b9968a686db0
nullFlow <ozmo765@gmail.com>
2021-08-22 14:35:07 +0200

    Task1-2 solution

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -215,22 +215,16 @@ Controller::Segment Controller::getNewHead() const
 
 void Controller::receive(std::unique_ptr<Event> e)
 {
-    try {
-        handleTimePassed(*dynamic_cast<EventT<TimeoutInd> const&>(*e));
-    } catch (std::bad_cast&) {
-        try {
-            handleDirectionChange(*dynamic_cast<EventT<DirectionInd> const&>(*e));
-        } catch (std::bad_cast&) {
-            try {
-                handleFoodPositionChange(*dynamic_cast<EventT<FoodInd> const&>(*e));
-            } catch (std::bad_cast&) {
-                try {
-                    handleNewFood(*dynamic_cast<EventT<FoodResp> const&>(*e));
-                } catch (std::bad_cast&) {
-                    throw UnexpectedEventException();
-                }
-            }
-        }
+    if(e->getMessageId() == TimeoutInd::MESSAGE_ID){
+        handleTimePassed(*static_cast<EventT<TimeoutInd> const&>(*e));
+    } else if(e->getMessageId() == DirectionInd::MESSAGE_ID){
+        handleDirectionChange(*static_cast<EventT<DirectionInd> const&>(*e));
+    } else if(e->getMessageId() == FoodInd::MESSAGE_ID){
+        handleFoodPositionChange(*static_cast<EventT<FoodInd> const&>(*e));
+    } else if(e->getMessageId() == FoodResp::MESSAGE_ID){
+        handleNewFood(*static_cast<EventT<FoodResp> const&>(*e));
+    } else {
+        throw UnexpectedEventException();
     }
 }
 
